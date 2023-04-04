8a14bb7b527778830a0ccfea7806ca4f632560d8
Ilayda Nur Guzel <ilaydanurguzel1@gmail.com>
2023-04-04 14:05:45 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+class Polygon{
+    constructor(Array){
+        this.Array=Array;
+    }
+    get countSides(){
+        return this.Array.length
+    }
+    get perimeter(){
+        let perimeter=0;
+        for(let sum of this.Array){
+            perimeter+=sum
+        }
+        return perimeter;
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        return (this.Array[0] + this.Array[1] > this.Array[2]) && (this.Array[1] + this.Array[2] > this.Array[0]) && (this.Array[0] + this.Array[2] > this.Array[1])? true : false;
+    }
+}
+class Square extends Polygon{
+    get isValid(){
+        return ((this.Array[0] === this.Array[1]) && (this.Array[0] === this.Array[2]) && (this.Array[0] === this.Array[3]))? true : false;
+    }
+    get area(){
+        return this.Array[0] ** 2;
+    }
+}
